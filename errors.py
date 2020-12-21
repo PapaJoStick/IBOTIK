1e48e9c178758e6cf8d48f83d875b56207a2655c
echevkin <echevkin@gmail.com>
2020-12-21 18:45:41 +0300

    ban kick error

--- errors.py
+++ errors.py
@@ -17,38 +17,6 @@ async def on_command_error(ctx, error):
 	print(f'обрабатываю {error}') # отчет в консоль
 
 
-@kick.error # обработка ошибки функции кика
-async def kick_error(ctx, error):
-	'''
-	функция обработки ошибки недостатка прав функции кика
-	аргумент ctx - контекст 
-	error - код ошибки
-	class discord.Embed - класс discord.py, отвечающий за embed'ы (другой способ красивого вывода текста)
-	class discord.Color - стандартная библиотека цветов discord.py
-	class discord.Member - класс discord.py, отвечающий за взаимодействие с участниками сервера и их данными
-	'''
-	if isinstance(error, commands.MissingPermissions): # проверка наличия ошибки и подохдит ли ошибка под искомую категорию
-		author = ctx.message.author # указание автора команды, при исполнении которой произошла ошибка
-		emb = discord.Embed(title = f'{author.name}, у вас недостаточно прав', colour = discord.Color.red()) # создание информации о причине ошибки
-		await ctx.channel.send(embed = emb) # вывод инфо о проблеме
- 
-
-@ban.error # обарботка ошибки функции бана
-async def ban_error(ctx, error):
-	'''
-	функция обработки ошибок недостатка прав функции бана
-	аргумент ctx - контекст
-	аргумент error - код ошибки
-	class discord.Embed - класс discord.py, отвечающий за embed'ы (другой способ красивого вывода текста)
-	class discord.Color - стандартная библиотека цветов discord.py
-	class discord.Member - класс discord.py, отвечающий за взаимодействие с участниками сервера и их данными
-	'''
-	if isinstance(error, commands.MissingPermissions): # проверка наличия ошибки и подходит ли ошибка под искомую категорию
-		author = ctx.message.author # указание автора команды с ошибкой
-		emb = discord.Embed(title = f'{author.name}, у вас недостаточно прав', colour = discord.Color.red()) # создания Embed с инфой о причине ошибки
-		await ctx.channel.send(embed = emb) # оправка инфо о причине ошибки
-
-
 @clear.error # обработка ошибки функции очистки
 async def clear_error(ctx, error):
 	'''
--- kick.py
+++ kick.py
@@ -24,3 +24,19 @@ async def kick(ctx, member: discord.Member, reason):
 	await member.send(f'Вы были кикнуты за {reason}') #отправляет причину кика на сервере забаненному
 	await member.kick(reason = reason) #кикает 
 	print(f'{member.name} кикнут')
+
+
+@kick.error # обработка ошибки функции кика
+async def kick_error(ctx, error):
+	'''
+	функция обработки ошибки недостатка прав функции кика
+	аргумент ctx - контекст 
+	error - код ошибки
+	class discord.Embed - класс discord.py, отвечающий за embed'ы (другой способ красивого вывода текста)
+	class discord.Color - стандартная библиотека цветов discord.py
+	class discord.Member - класс discord.py, отвечающий за взаимодействие с участниками сервера и их данными
+	'''
+	if isinstance(error, commands.MissingPermissions): # проверка наличия ошибки и подохдит ли ошибка под искомую категорию
+		author = ctx.message.author # указание автора команды, при исполнении которой произошла ошибка
+		emb = discord.Embed(title = f'{author.name}, у вас недостаточно прав', colour = discord.Color.red()) # создание информации о причине ошибки
+		await ctx.channel.send(embed = emb) # вывод инфо о проблеме
